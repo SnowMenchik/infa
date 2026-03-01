57d7d6e41ced85c6a6b004463b0248a3af438947
zenkinartyom11-sys <zenkin.artyom11@gmail.com>
2026-03-01 13:21:20 +0500

    Update 1.py

--- 23/Количество программ в которых есть одновременно + и -/1.py
+++ 23/Количество программ в которых есть одновременно + и -/1.py
@@ -1,5 +1,9 @@
+from functools import *
+
+@lru_cache(None)
+
 def f(x, y):
     if x==y: return 1
-    if x>y +1 or x%3 ==0: return 0
+    if x>y +100 or x%3 ==0: return 0
     return f(x-1, y) + f(x+3, y) + f(x*2, y)
 print(f(5, 100))
