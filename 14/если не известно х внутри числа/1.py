4996fda3513d39ff2aae82fe1aed5f1fd9a4cdb6
zenkinartyom11-sys <zenkin.artyom11@gmail.com>
2026-03-01 15:35:55 +0500

    Update 1.py

--- 14/если не известно х внутри числа/1.py
+++ 14/если не известно х внутри числа/1.py
@@ -1,6 +1,6 @@
 s = sorted('0123456789QWERTYUIOPASDFGHJKLZXCVBNM')
 for x in s[:29]:
-    n1 = int('465' + str(x) + '7921', 29)
+    n1 = int('463' + str(x) + '7921', 29)
     n2 = int('8241' + str(x) + '153', 29)
     num =  n1 + n2
     if num % 28 == 0:
