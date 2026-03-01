d29fa3dbceed5ff814dc61f78ddcec1ff810f76f
zenkinartyom11-sys <zenkin.artyom11@gmail.com>
2026-03-01 15:48:19 +0500

    Update 1.py

--- 16/рекурсия/1.py
+++ 16/рекурсия/1.py
@@ -1,13 +1,22 @@
 from functools import *
+
 @lru_cache(None)
 def f(n):
-    if n == 0:
-        return 1
-    if n>0  and n % 2 !=0:
-        return f(n//100) * (n%10)
-    if n >0 and n %2 ==0:
-        return f(n//100)
+    if n < 31054:
+        return f(n+4) + 3020
+    else:
+        return 3 * (g(n-2) -15)
 
-for i in range(1234, 12345):
+@lru_cache(None)
+def g(n):
+    if n >= 28:
+        return g(n-5) - 15
+    else:
+        return 3 *n - 4
+
+for i in range(400000):
+    g(i)
+for i in range(400000, 0 ,-1):
     f(i)
-print(f(1239) - f(123))
+
+print(f(15))
