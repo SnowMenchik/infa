fea4f604ae3a1b50ed97827e3068c40efbf09d2e
SnowMenchik <stanky2stanok@gmail.com>
2026-03-09 15:29:41 +0500

    Изменение структуры гита

--- 14/если не известна система счисления/1.py
+++
@@ -1,8 +0,0 @@
-for i in range(18, 50):
-    n1 = int('22A12E', i)
-    n2 = int('2F1391', i)
-    n3 = int('1H05D0', i)
-    num = n1 + n2 - n3
-    if num % 19 == 0:
-        print(num // 19)
-        break
